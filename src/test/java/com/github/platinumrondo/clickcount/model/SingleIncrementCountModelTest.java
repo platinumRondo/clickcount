294e2f04a777a1f67973c7e8d71b8199d6032d8a
Platinum Rondo <platinumrondo@gmail.com>
2015-12-25 19:31:47 +0100

    Test the constructor with initial value.

--- src/test/java/com/github/platinumrondo/clickcount/model/SingleIncrementCountModelTest.java
+++ src/test/java/com/github/platinumrondo/clickcount/model/SingleIncrementCountModelTest.java
@@ -4,12 +4,22 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import java.util.Random;
+
 public class SingleIncrementCountModelTest {
     private SingleIncrementCountModel model;
     
     @Before
     public void setUp() {
         model = new SingleIncrementCountModel();
+    }
+    
+    @Test
+    public void testConstructorWithInitialValue() {
+        Random r = new Random();
+        int initialValue = r.nextInt();
+        model = new SingleIncrementCountModel(initialValue);
+        assertEquals(initialValue, model.get());
     }
     
     @Test
