b90d14d9f593efebf75c7d6173106d07a9fc83ea
Platinum Rondo <platinumrondo@gmail.com>
2015-12-25 19:24:58 +0100

    The default theme is changed to Nimbus upon start.

--- src/main/java/com/github/platinumrondo/clickcount/Main.java
+++ src/main/java/com/github/platinumrondo/clickcount/Main.java
@@ -6,6 +6,9 @@ import java.awt.event.WindowListener;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import com.github.platinumrondo.clickcount.model.CountModel;
 import com.github.platinumrondo.clickcount.model.SingleIncrementCountModel;
@@ -14,6 +17,7 @@ import com.github.platinumrondo.clickcount.view.CountView;
 public class Main {
 
     public static void main(String[] args) {
+        changeLAF();
         long value = CountPreffer.get();
         CountModel count = new SingleIncrementCountModel(value);
         WindowListener wl = new WindowAdapter() {
@@ -30,6 +34,23 @@ public class Main {
             frame.addWindowListener(wl);
             frame.setVisible(true);
         });
+    }
+
+    private static void changeLAF() {
+        // taken
+        try {
+            for (LookAndFeelInfo lafi : UIManager.getInstalledLookAndFeels()) {
+                if (lafi.getClassName().equals("Nimbus")) {
+                    UIManager.setLookAndFeel(lafi.getClassName());
+
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException
+                | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            System.err.println("There was a problem setting the Nimbus theme.");
+        }
+
     }
 
 }
